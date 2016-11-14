c75c1f72fcfd88b4bde81bba78cfe237bad6754d
sheguoguo <sluffy@126.com>
2016-11-14 17:17:25 +0800

    no message

--- dataframefeed/my_test.py
+++ dataframefeed/my_test.py
@@ -8,8 +8,10 @@ This is a temporary script file.
 #转换tushare数据到yahoo数据格式的csv文件
 
 #==============================================================================
+# yt=ts.get_hist_data('600066')
+# yt.to_csv('../data/600066.csv')
 # import data_utf8 as d8
-# d8.change_code_type_to_yahoo('600029')
+# d8.change_code_type_to_yahoo('600066')
 #==============================================================================
 
 
@@ -18,7 +20,6 @@ from pyalgotrade.technical import ma
 from pyalgotrade.technical import cross
 from pyalgotrade import plotter
 from pyalgotrade.barfeed import yahoofeed
-from pyalgotrade.stratanalyzer import returns
 from pyalgotrade.stratanalyzer import returns
 from pyalgotrade.stratanalyzer import sharpe
 from pyalgotrade.stratanalyzer import drawdown
@@ -33,29 +34,54 @@ class SMACrossOver(strategy.BacktestingStrategy):
         self.setUseAdjustedValues(False)
         self.__prices = feed[instrument].getPriceDataSeries()
         self.__sma = ma.SMA(self.__prices, smaPeriod)
+        self.__total_position=0
+        self.__avg_price=0
+        self.__buy_flag=False
+        
         
           
     def getSMA(self):
         return self.__sma
     
     def onEnterCanceled(self, position):
-        self.__position = None
+        pass
 
     def onExitOk(self, position):
-        self.__position = None
+        self.__total_position-=position.getShares()
+        
+    def onEnterOk(self,position):
+        self.__total_position+=position.getShares()
+        if self.getBroker().getShares(self.__instrument)>0:
+            self.__avg_price=(self.__position.getEntryOrder().getAvgFillPrice()*self.__position.getShares()+self.__avg_price*(self.getBroker().getShares(self.__instrument)-self.__position.getShares()))/self.__total_position
+        else:
+            self.__avg_price=0
+        print self.__avg_price
+#==============================================================================
+#         print self.__position.getShares()
+#         print self.getBroker().getCash()
+#         print self.getBroker().getShares(self.__instrument)
+#         print self.__position.getEntryOrder().getAvgFillPrice()
+#         print (self.__position.getEntryOrder().getAvgFillPrice()*self.__position.getShares()+self.__avg_price*(self.getBroker().getShares(self.__instrument)-self.__position.getShares()))/self.getBroker().getShares(self.__instrument)
+#==============================================================================
+#        self.__position=self.getBroker().getPositions()
+        
 
     def onExitCanceled(self, position):
         # If the exit was canceled, re-submit it.
-        self.__position.exitMarket()
+#        self.__position.exitMarket()
+        pass
 
     def onBars(self, bars):
         # If a position was not opened, check if we should enter a long position.
-        cs=0
-        if self.__position is None:
+        
+        if self.getBroker().getShares(self.__instrument)==0:
             if cross.cross_above(self.__prices, self.__sma) > 0:
-                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+#                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+                shares=1000
                 # Enter a buy market order. The order is good till canceled.
                 self.__position = self.enterLong(self.__instrument, shares, True)
+                self.__avg_price=bars[self.__instrument].getPrice()
+                print 'init:  buy 1000    price:'+str(bars[self.__instrument].getPrice())
 #                self.__position = self.enterLong(self.__instrument, 1000, True)
 #                order_cs=self.__position.getEntryOrder()
 #                cs=self.__position.getReturn()
@@ -68,36 +94,40 @@ class SMACrossOver(strategy.BacktestingStrategy):
 #                 
 #                 self.__position.exitMarket()
 #==============================================================================
-                
-        elif not self.__position.exitActive() and (self.__position.getEntryOrder().getAvgFillPrice()*0.92>bars[self.__instrument].getPrice() or self.__position.getEntryOrder().getAvgFillPrice()*1.2<bars[self.__instrument].getPrice()):
-                print ("%s,%s" % (self.__position.getEntryOrder().getAvgFillPrice(), bars[self.__instrument].getPrice()))
-                self.__position.exitMarket()
+        elif self.getBroker().getShares(self.__instrument)>1000 and self.__avg_price<bars[self.__instrument].getPrice() and self.__buy_flag:
+            self.__position=self.enterShort(self.__instrument,self.getBroker().getShares(self.__instrument)/2,True)
+            self.__buy_flag=False
+            print 'party sells:'+str(-1*self.getBroker().getShares(self.__instrument))+'   avg_price:'+str(self.__avg_price)+'   current price:'+str(bars[self.__instrument].getPrice())
+        elif self.getBroker().getShares(self.__instrument)>0 and self.__avg_price*0.92>bars[self.__instrument].getPrice() and self.getBroker().getShares('south_air')*bars[self.__instrument].getPrice()<self.getBroker().getCash():
+             self.__position=self.enterLong(self.__instrument, self.getBroker().getShares(self.__instrument), True)
+             self.__buy_flag=True
+             print 'add buy:'+str(self.getBroker().getShares(self.__instrument))+'   avg_price:'+str(self.__avg_price)+'   current price:'+str(bars[self.__instrument].getPrice())
+        elif self.getBroker().getShares(self.__instrument)>0 and (self.__avg_price*0.92>bars[self.__instrument].getPrice() or self.__avg_price*1.2<bars[self.__instrument].getPrice()):
+            #print 'loss now price:'+str(bars[self.__instrument].getPrice())
+            print 'sells:'+str(-1*self.getBroker().getShares(self.__instrument))+'   avg_price:'+str(self.__avg_price)+'   current price:'+str(bars[self.__instrument].getPrice())
+            #self.getBroker().createMarketOrder(broker.Order.Action.SELL, self.__instrument, self.__total_position, False)
+            #self.marketOrder(self.__instrument,-1*int(self.getBroker().getShares(self.__instrument)),True,True)
+            self.__position=self.enterShort(self.__instrument,self.getBroker().getShares(self.__instrument),True)
+            print self.__total_position
 
 
 
 feed = yahoofeed.Feed()
-feed.addBarsFromCSV("south_air", "../data_yahoo/600029.csv")
+feed.addBarsFromCSV("south_air", "../data_yahoo/600066.csv")
 
 # Evaluate the strategy with the feed's bars.
 myStrategy = SMACrossOver(feed, "south_air", 20)
 
 # Attach a returns analyzers to the strategy.
-returnsAnalyzer = returns.Returns()
-myStrategy.attachAnalyzer(returnsAnalyzer)
+#==============================================================================
+# returnsAnalyzer = returns.Returns()
+# myStrategy.attachAnalyzer(returnsAnalyzer)
+#==============================================================================
 
-# Attach the plotter to the strategy.
-plt = plotter.StrategyPlotter(myStrategy)
-# Include the SMA in the instrument's subplot to get it displayed along with the closing prices.
-plt.getInstrumentSubplot("south_air").addDataSeries("SMA", myStrategy.getSMA())
-# Plot the simple returns on each bar.
-plt.getOrCreateSubplot("returns").addDataSeries("Simple returns", returnsAnalyzer.getReturns())
 
-# Run the strategy.
-myStrategy.run()
-myStrategy.info("Final portfolio value: $%.2f" % myStrategy.getResult())
 
-# Plot the strategy.
-plt.plot()
+
+
 retAnalyzer = returns.Returns()
 myStrategy.attachAnalyzer(retAnalyzer)
 sharpeRatioAnalyzer = sharpe.SharpeRatio()
@@ -107,7 +137,24 @@ myStrategy.attachAnalyzer(drawDownAnalyzer)
 tradesAnalyzer = trades.Trades()
 myStrategy.attachAnalyzer(tradesAnalyzer)
 
+
+# Attach the plotter to the strategy.
+plt = plotter.StrategyPlotter(myStrategy)
+# Include the SMA in the instrument's subplot to get it displayed along with the closing prices.
+plt.getInstrumentSubplot("south_air").addDataSeries("SMA", myStrategy.getSMA())
+# Plot the simple returns on each bar.
+plt.getOrCreateSubplot("returns").addDataSeries("Simple returns", retAnalyzer.getReturns())
+
+
+
 # Run the strategy.
+myStrategy.run()
+#myStrategy.info("Final portfolio value: $%.2f" % myStrategy.getResult())
+
+# Plot the strategy.
+plt.plot()
+
+
 
 print("Final portfolio value: $%.2f" % myStrategy.getResult())
 #print("Cumulative returns: %.2f %%" % (retAnalyzer.getCumulativeReturns()[-1] * 100))
